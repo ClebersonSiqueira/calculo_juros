bb6d4c87d622ad5e5fcefd965cd5d2fbd7789cc1
ClebersonSiqueira <66030676+ClebersonSiqueira@users.noreply.github.com>
2020-05-27 14:48:03 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Http.Headers;
+
+namespace calculo_juros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mes = 1;
+            double valor = 0.0;
+            Console.WriteLine("Quantos meses calcular?");
+            int mesUsuario = int.Parse(Console.ReadLine());
+            Console.WriteLine("Porcentagem do ganho mensal?");
+            double Porcento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Porcento = Porcento / 100;
+            Console.WriteLine("Vai depositar quanto por mes?");
+            double valorDepositado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while(mes <= mesUsuario)
+            {
+                valor = valor + (valor * Porcento);
+                valor += valorDepositado;
+                Console.WriteLine("Meses deccoridos: " + mes + ", Valor em conta " + valor.ToString("F2", CultureInfo.InvariantCulture));
+                mes++;
+            }
+            double juros = valor - (valorDepositado * mesUsuario);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Valor total em " + mesUsuario + " meses: " + valor.ToString("F2", CultureInfo.InvariantCulture) + ". Valores ganhos em juros: " + juros);
+        }
+    }
+}
