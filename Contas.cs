48f1769876ab5487e4f3f71499163cf0ee444d64
ClebersonSiqueira <66030676+ClebersonSiqueira@users.noreply.github.com>
2020-05-27 16:37:23 -0300

    Add files via upload

---
+++ Contas.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace calculo_juros
+{
+    class Contas
+    {
+        private double Total { get; set; }
+        private int i = 1;
+        private double juros;
+        public double Porcentagem { get; set; }
+        public double ValorDepositado { get; set; }
+        public int Meses { get; set; }
+        
+
+
+
+
+        public Contas(int meses, double porcentagem, double valorDepositados)
+        {
+            Porcentagem = porcentagem;
+            ValorDepositado = valorDepositados;
+            Meses = meses;
+        }
+
+        private void Calculo()
+        {
+
+            for (i = 1; i <= Meses; i++)
+            {
+                Total = Total + (Total * Porcentagem);
+                Total += ValorDepositado;
+                Console.WriteLine("Meses deccoridos: " + i + ", Valor em conta " + Total.ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+
+        private void rendimentos()
+        {
+            juros = Total - (ValorDepositado * Meses);
+        }
+
+        public override string ToString()
+        {
+            Calculo();
+            rendimentos();
+            return "Valor total em " + Meses + " meses: " + Total.ToString("F2", CultureInfo.InvariantCulture) + ". Valores ganhos em juros: " + juros.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,8 +8,6 @@ namespace calculo_juros
     {
         static void Main(string[] args)
         {
-            int mes = 1;
-            double valor = 0.0;
             Console.WriteLine("Quantos meses calcular?");
             int mesUsuario = int.Parse(Console.ReadLine());
             Console.WriteLine("Porcentagem do ganho mensal?");
@@ -18,17 +16,9 @@ namespace calculo_juros
             Console.WriteLine("Vai depositar quanto por mes?");
             double valorDepositado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            while(mes <= mesUsuario)
-            {
-                valor = valor + (valor * Porcento);
-                valor += valorDepositado;
-                Console.WriteLine("Meses deccoridos: " + mes + ", Valor em conta " + valor.ToString("F2", CultureInfo.InvariantCulture));
-                mes++;
-            }
-            double juros = valor - (valorDepositado * mesUsuario);
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Valor total em " + mesUsuario + " meses: " + valor.ToString("F2", CultureInfo.InvariantCulture) + ". Valores ganhos em juros: " + juros);
+            Contas c = new Contas(mesUsuario, Porcento, valorDepositado);
+
+            Console.WriteLine(c);
         }
     }
 }
