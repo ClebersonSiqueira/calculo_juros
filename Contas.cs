247a94c81fcc652ace1a23be78eed54404da9af9
ClebersonSiqueira <66030676+ClebersonSiqueira@users.noreply.github.com>
2020-05-28 16:39:20 -0300

    Atualizacao 0.1.1
    Ajustes nos cálculos do ganhos totais e adição de um relatório mês a mês.

--- Contas.cs
+++ Contas.cs
@@ -9,45 +9,79 @@ namespace calculo_juros
     class Contas
     {
         private double Total { get; set; }
-        private int i = 1;
-        private double juros;
+        private double juros, capitalAplicado, descontoIr;
         public double Porcentagem { get; set; }
         public double ValorDepositado { get; set; }
         public int Meses { get; set; }
-        
+        public double IR { get; set; }
 
 
 
 
-        public Contas(int meses, double porcentagem, double valorDepositados)
+
+        public Contas(int meses, double porcentagem, double valorDepositados, double ir)
         {
             Porcentagem = porcentagem;
             ValorDepositado = valorDepositados;
             Meses = meses;
+            IR = ir;
         }
 
-        private void Calculo()
+        public void Calculo_periodo_completo()
         {
+            capitalAplicado = ValorDepositado * (Meses + 1);
+            Total = ValorDepositado * (Math.Pow(Porcentagem + 1, Meses + 1) - 1);
+            Total = Total / Porcentagem;
+            descontoIr = (Total - capitalAplicado) * IR; // calcula 1/4 de IR em cima dos ganhos
+            Total -= descontoIr;
+            juros = Total - capitalAplicado;
+            Total -= ValorDepositado;
+            Console.WriteLine("Valor total em " + Meses + " meses: "
+                + Total.ToString("F2", CultureInfo.InvariantCulture) + ". Valores ganhos em juros: "
+                + juros.ToString("F2", CultureInfo.InvariantCulture) + ", Desconto de IR: "
+                + descontoIr.ToString("F2", CultureInfo.InvariantCulture));
+        }
 
-            for (i = 1; i <= Meses; i++)
+        public void Calculo_mensal()
+        {
+            
+            Total = 0.0;
+            for (int i = 1; i <= Meses; i++)
             {
-                Total = Total + (Total * Porcentagem);
-                Total += ValorDepositado;
-                Console.WriteLine("Meses deccoridos: " + i + ", Valor em conta " + Total.ToString("F2", CultureInfo.InvariantCulture));
+                capitalAplicado = ValorDepositado * (i + 1);
+                Total = ValorDepositado * (Math.Pow(Porcentagem + 1, i + 1) - 1);
+                Total = Total / Porcentagem;
+                descontoIr = (Total - capitalAplicado) * IR; // calcula 1/4 de IR em cima dos ganhos
+                Total -= descontoIr;
+                juros = Total - capitalAplicado;
+                Total -= ValorDepositado;
+
+                Console.WriteLine("Meses decorridos: " + i + ", Valor em conta "
+                    + Total.ToString("F2", CultureInfo.InvariantCulture));
             }
+            
+
+            // Tem esse modo abaixo, que ficaria mais correto, pois calcula o lucro realmente mes a mes
+            /*
+            Total = 0.0;
+            for (int i = 1; i <= Meses; i++)
+            {
+                Total += ValorDepositado;
+                double totalAnterior = Total;
+                Total = Total * (1 + Porcentagem); //Calcula a % de ganhos em cima do total dos meses anteriores
+                double ganhos = Total - totalAnterior;
+                descontoIr = ganhos * IR;
+                Total -= descontoIr;
+                ganhos = Total - totalAnterior;
+                Console.WriteLine("Meses deccoridos: " + i + ", Valor em conta " + Total.ToString("F2", CultureInfo.InvariantCulture)
+                    + ", ganhos no mes: " + ganhos.ToString("F2", CultureInfo.InvariantCulture) 
+                    + ", Valor de IR do mes: " + descontoIr.ToString("F2", CultureInfo.InvariantCulture));
+            }*/
         }
 
-        private void rendimentos()
-        {
-            juros = Total - (ValorDepositado * Meses);
-        }
 
-        public override string ToString()
-        {
-            Calculo();
-            rendimentos();
-            return "Valor total em " + Meses + " meses: " + Total.ToString("F2", CultureInfo.InvariantCulture) + ". Valores ganhos em juros: " + juros.ToString("F2", CultureInfo.InvariantCulture);
-        }
+
+
 
 
     }
--- Program.cs
+++ Program.cs
@@ -12,13 +12,21 @@ namespace calculo_juros
             int mesUsuario = int.Parse(Console.ReadLine());
             Console.WriteLine("Porcentagem do ganho mensal?");
             double Porcento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Porcento = Porcento / 100;
+            Porcento = Porcento / 100; // transforma % em decimal
             Console.WriteLine("Vai depositar quanto por mes?");
             double valorDepositado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Qual a porcentagem do IR?");
+            double ir = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ir = ir / 100; // transforma IR em decimal
 
-            Contas c = new Contas(mesUsuario, Porcento, valorDepositado);
+            Contas c = new Contas(mesUsuario, Porcento, valorDepositado, ir);
 
-            Console.WriteLine(c);
+            c.Calculo_periodo_completo();
+
+            Console.WriteLine("Gostaria de mostrar o relatorio mes a mes? S/N");
+            string resposta = Console.ReadLine();
+            if (resposta == "s" || resposta == "S")
+                c.Calculo_mensal();
         }
     }
 }
